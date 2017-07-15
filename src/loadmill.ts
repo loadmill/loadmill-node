1d08ad0962d490a40828f2d9726c8d30426d7434
itaysabato <itaysabato@gmail.com>
2017-07-15 16:57:59 +0300

    Help changes.

--- src/loadmill.ts
+++ src/loadmill.ts
@@ -2,11 +2,14 @@ import * as program from 'commander';
 
 program
     .usage("<config-file> -t <token> [options] [parameter=value...]")
-    .description("Launch a load test or run a functional test on loadmill.com. " +
-        "You may assign parameter values by passing arguments such as 'loadmill test.json host=www.myapp.com port=80'.")
+    .description(
+        "Run a load test or a functional test on loadmill.com.\n  " +
+        "You may set parameter values by passing space-separated 'name=value' pairs, e.g. 'host=www.myapp.com port=80'.\n\n  " +
+        "Learn more at https://www.npmjs.com/package/loadmill#cli"
+    )
     .option("-t, --token <token>", "Loadmill API Token. You must provide a token in order to run tests.")
     .option("-l, --load-test", "Launch a load test. If not set, a functional test will run instead.")
-    .option("-w, --wait", "Wait for the load test to finish.")
+    .option("-w, --wait", "Wait for the load test to finish. Functional tests are always waited on.")
     .parse(process.argv);
 
 const {
